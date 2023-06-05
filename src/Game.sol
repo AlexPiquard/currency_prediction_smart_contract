51af53e77e68458a0f967360754554641ab8d38e
Alex Piquard <alex.piquard@hotmail.com>
2023-06-05 09:53:44 +0200

    chore: chainlink

--- src/Game.sol
+++ src/Game.sol
@@ -2,17 +2,36 @@
 pragma solidity ^0.8.20;
 
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Game is Ownable {
-    uint256 public constant KEEP_PERCENT = 0.05;
-    mapping(address => mapping(uint256 => mapping(string => uint8))) public bets;
+    AggregatorV3Interface internal dataFeed;
+    uint256 public constant KEEP_PERCENT = 5;
+    mapping(address => mapping(string => mapping(uint8 => uint256))) public bets;
 
-    function betIncrease(uint256 amount, string currency) public payable {
-        bets[msg.sender][currency][1] = amount;
+    constructor() {
+        dataFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
     }
 
-    function betDecrease(uint256 amount, string currency) public payable {
-        bets[msg.sender][currency][0] = amount;
+    function getLatestData() public view returns (int) {
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
+
+    function betIncrease(uint256 amount, string calldata currency) public payable {
+        bets[msg.sender][currency][uint8(1)] = amount;
+    }
+
+    function betDecrease(uint256 amount, string calldata currency) public payable {
+        bets[msg.sender][currency][uint8(0)] = amount;
     }
 
 }
