464a37e35bf32c9a65e19201c11d18f6a38489db
AlexPiquard <alex.piquard@hotmail.com>
2023-06-10 12:16:40 +0200

    fix: build

--- src/Game.sol
+++ src/Game.sol
@@ -21,7 +21,9 @@ contract Game is Ownable {
     struct Currency {
         AggregatorV3Interface dataFeed;
         uint80 lastRoundID;
+        uint80 currentRoundID;
         int lastPrice;
+        int currentPrice;
     }
 
     // @dev Iterable list of currency keys.
@@ -34,71 +36,103 @@ contract Game is Ownable {
     mapping(address => Bet) private bets;
 
     constructor() {
-        mapping(string => Currency) currenciesTmp;
-        // Get default data for all supported currencies.
-        currenciesTmp["BTC"] = getLatestCurrencyData(AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43));
-        currenciesTmp["ETH"] = getLatestCurrencyData(AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306));
-        currenciesTmp["EUR"] = getLatestCurrencyData(AggregatorV3Interface(0x1a81afB8146aeFfCFc5E50e8479e826E7D55b910));
-        currencies = currenciesTmp;
+        // @dev Get default data for all supported currencies.
+        initializeCurrencyData("BTC", AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43));
+        initializeCurrencyData("ETH", AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306));
+        initializeCurrencyData("EUR", AggregatorV3Interface(0x1a81afB8146aeFfCFc5E50e8479e826E7D55b910));
     }
 
-    function getLatestCurrencyData(AggregatorV3Interface dataFeed) private view returns (Currency) {
+    function initializeCurrencyData(string memory currencyKey, AggregatorV3Interface dataFeed) private {
         (
             uint80 roundID,
-            int answer
-        ) = currency.dataFeed.latestRoundData();
-        return Currency(dataFeed, roundID, answer);
+            int answer,
+            /*uint256 startedAt*/,
+            uint256 updatedAt,
+            /*uint80 answeredInRound*/
+        ) = (dataFeed).latestRoundData();
+
+        // @dev If the round is not complete yet, updatedAt is 0
+        require(updatedAt > 0, "Round not complete");
+
+        currencies[currencyKey] = Currency(dataFeed, 0, roundID, 0, answer);
     }
 
-    function betIncrease(uint256 amount, string calldata currency) public payable {
-        bet(amount, 1, currency);
+    function updateLatestCurrencyData(string memory currencyKey) private {
+        Currency memory currency = currencies[currencyKey];
+        (
+            uint80 roundID,
+            int answer,
+            /*uint256 startedAt*/,
+            uint256 updatedAt,
+            /*uint80 answeredInRound*/
+        ) = (currency.dataFeed).latestRoundData();
+
+        // @dev If the round is not complete yet, updatedAt is 0
+        require(updatedAt > 0, "Round not complete");
+
+        currency.lastRoundID = currency.currentRoundID;
+        currency.lastPrice = currency.currentPrice;
+        currency.currentRoundID = roundID;
+        currency.currentPrice = answer;
+
+        currencies[currencyKey] = currency;
     }
 
-    function betDecrease(uint256 amount, string calldata currency) public payable {
-        bet(amount, 0, currency);
+    function betIncrease(uint256 amount, string memory currency) public payable {
+        bet(amount, true, currency);
     }
 
-    function bet(uint256 amount, bool state, string calldata currency) private {
-        Bet bet = bets[msg.sender];
-        if (bet == address(0x0)) {
-            bet = Bet(currency, state, amount);
-            bets[msg.sender] = bet;
+    function betDecrease(uint256 amount, string memory currency) public payable {
+        bet(amount, false, currency);
+    }
+
+    function getBetAmount() public view returns (uint256) {
+        return bets[msg.sender].amount;
+    }
+
+    function getBetCurrency() public view returns (string memory) {
+        return bets[msg.sender].currency;
+    }
+
+    function getBetEvolution() public view returns (bool) {
+        return bets[msg.sender].bet;
+    }
+
+    function bet(uint256 amount, bool state, string memory currency) private {
+        Bet memory b = bets[msg.sender];
+        if (b.amount == 0) {
+            b = Bet(currency, state, amount);
+            bets[msg.sender] = b;
         } else {
-            require(bet.bet == state, "cant change bet");
-            require(bet.currency == currency, "cant change currency");
-            bet.amount += amount;
+            require(b.bet == state, "cant change bet");
+            require(keccak256(bytes(b.currency)) == keccak256(bytes(currency)), "cant change currency");
+            b.amount += amount;
         }
     }
 
     // @notice Everyone can generate result, at his own cost.
     function result() public {
-        // @dev Retrieve price state for each currency, and filter unchanged.
-        mapping(string => bool) currencyStates;
-
-        for (int i = 0; i < currencyKeys; ++i) {
-            string currencyKey = currencyKeys[i];
-            Currency oldCurrency = currencies[currencyKey];
-            Currency currency = getLatestCurrencyData(oldCurrency.dataFeed);
-
-            // @dev If roundID is still the same, do nothing.
-            if (currency.lastRoundID == oldCurrency.lastRoundID) continue;
-
-            // @dev Check if price increased or not.
-            currencyStates[currencyKey] = currency.lastPrice > oldCurrency.lastPrice;
+        // @dev Update data for all currencies.
+        for (uint256 i = 0; i < currencyKeys.length; ++i) {
+            updateLatestCurrencyData(currencyKeys[i]);
         }
 
         // @dev Get winners and sum bet amount.
-        address[] winners = new address[];
+        address[] memory winners = new address[](users.length);
         uint256 winnersBalance;
 
-        for (int i = 0; i < users.length; ++i) {
-            Bet bet = bets[users[i]];
+        for (uint256 i = 0; i < users.length; ++i) {
+            Bet memory b = bets[users[i]];
+            Currency memory currency = currencies[b.currency];
+
+            // @dev If round is still the same for this currency, we do nothing.
+            if (currency.lastRoundID == currency.currentRoundID) continue;
 
             // @dev Check if he's wrong.
-            if (bet.bet != currencyStates[bet.currency]) continue;
+            if (b.bet != (currency.currentPrice > currency.lastPrice)) continue;
 
-            winners.push(users[i]);
-            winnersBalance += bet.amount;
+            winners[i] = users[i];
+            winnersBalance += b.amount;
         }
 
         // @dev Get balance and subtract contract percent.
@@ -108,14 +142,13 @@ contract Game is Ownable {
         winnersBalance -= ownerGain;
 
         // @dev Give money to winners.
-        for (int i = 0; i < winners.length; ++i) {
-            Bet bet = bets[winners[i]];
+        for (uint256 i = 0; i < winners.length; ++i) {
+            if (winners[i] == address(0)) continue;
 
-            // @dev Check if he's wrong.
-            if (bet.bet != currencyStates[bet.currency]) continue;
+            Bet memory b = bets[winners[i]];
 
             // @dev Give percent of balance to user, related to bet amount.
-            payable(address(winners[i])).transfer((bet.amount*balance)/winnersBalance);
+            payable(address(winners[i])).transfer((b.amount*balance)/winnersBalance);
         }
 
         // @dev The rest goes to owner.
@@ -124,8 +157,10 @@ contract Game is Ownable {
 
     // @dev Reset game at end of round, after result.
     function reset() private {
+        for (uint256 i = 0; i < users.length; ++i) {
+            delete bets[users[i]];
+        }
         delete users;
-        delete bets;
     }
 
 }
